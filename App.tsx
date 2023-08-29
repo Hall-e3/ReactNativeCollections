351e612deccd06737c1478c73ea4b47e52e08114
Hall-e3 <hallenochasanda@gmail.com>
2023-08-29 23:47:12 +0300

    new setup changes

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+type Props = {};
+
+const App=(props:Props)=> {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
